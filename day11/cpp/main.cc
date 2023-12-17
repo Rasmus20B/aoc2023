f294a8e7ec1e1fcd7278aae5a8dd75e050b0785a
James Tateossian <jmtateossian50b@gmail.com>
2023-12-17 19:11:06 +0000

    washed day11 a bit

--- day11/cpp/main.cc
+++ day11/cpp/main.cc
@@ -1,6 +1,10 @@
 
 
+#include <cassert>
 #include <cmath>
+#include <cstdint>
+#include <future>
+#include <thread>
 #include <iostream>
 #include <string>
 #include <string_view>
@@ -9,6 +13,20 @@
 #include <unordered_set>
 
 #include "aoc.h"
+
+[[gnu::const]]
+auto calc_distances(const std::vector<int64_t>& dim, const int multiple) -> size_t {
+  int64_t sum = 0;
+  int64_t empty = 0;
+  int64_t p = 0;
+  for(auto d: dim) {
+    sum += d * empty;
+    p += d;
+    auto m = ((d == 0) * multiple) + 1;
+    empty += p * std::min(m, multiple);
+  }
+  return sum;
+}
 
 auto part1(std::string_view data, int multiple) -> size_t {
 
@@ -22,46 +40,25 @@ auto part1(std::string_view data, int multiple) -> size_t {
 
   for(size_t i = 0; i < rows.size(); ++i) {
     for(size_t j = 0; j < cols.size(); ++j) {
-      if(universe[i][j] == '.') {
+      if(universe[i][j] == '.') [[likely]]{
         continue;
       }
       rows[i]++;
       cols[j]++;
     }
   }
-
-  int64_t sum = 0;
-  int64_t empty = 0;
-  int64_t p = 0;
-  for(auto r: rows) {
-    if(r > 0) {
-      sum += r * empty;
-      p += r;
-      empty += p;
-    } else {
-      empty += multiple * p;
-    }
-  }
-
-  empty = 0;
-  p = 0;
-  for(auto c: cols) {
-    if(c) {
-      sum += c * empty;
-      p += c;
-      empty += p;
-    } else {
-      empty += multiple * p;
-    }
-  }
-  return sum;
+  return calc_distances(rows, multiple) + calc_distances(cols, multiple);
 }
+
+
 auto main() -> int {
   auto content = get_file_as_string("../data.txt");
-  printf("%ld\n", part1("...#......\n.......#..\n#.........\n..........\n......#...\n.#........\n.........#\n..........\n.......#..\n#...#.....", 2));
-  printf("%ld\n", part1("...#......\n.......#..\n#.........\n..........\n......#...\n.#........\n.........#\n..........\n.......#..\n#...#.....", 1000000));
-  std::cout << "Silver: " << part1(content, 2) << "\n";
-  std::cout << "Gold: " << part1(content, 1000000) << "\n";
+  assert(part1(content, 2) == 17341358);
+  assert(part1(content, 1000000) == 805925729538);
+  // printf("%ld\n", part1("...#......\n.......#..\n#.........\n..........\n......#...\n.#........\n.........#\n..........\n.......#..\n#...#.....", 2));
+  // printf("%ld\n", part1("...#......\n.......#..\n#.........\n..........\n......#...\n.#........\n.........#\n..........\n.......#..\n#...#.....", 1000000));
+  // std::cout << "Silver: " << part1(content, 2) << "\n";
+  // std::cout << "Gold: " << part1(content, 1000000) << "\n";
 
   return 0;
 }
